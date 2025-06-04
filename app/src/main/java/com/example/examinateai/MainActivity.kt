ad6d997af0fc723f2c7daa9acd849be6e38f0db8
IsmailAbbasi <ismailabbasi118@gmail.com>
2025-06-04 20:09:58 +0530

    Fixed issue of back button now on pressing back button it is goig back on prev screen

--- app/src/main/java/com/example/examinateai/MainActivity.kt
+++ app/src/main/java/com/example/examinateai/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.examinateai
 
 import android.os.Bundle
+import android.webkit.WebResourceRequest
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -15,12 +17,32 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         webView = findViewById(R.id.webview)
-        webView.webViewClient = WebViewClient()
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                view?.loadUrl(request?.url.toString())
+                return true
+            }
+        }
 
         val webSettings: WebSettings = webView.settings
         webSettings.javaScriptEnabled = true
+        webSettings.domStorageEnabled = true
         webSettings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
 
-        webView.loadUrl("https://examinateai.com/")
+        webView.loadUrl("https://examinateai.com")
+
+        // ✅ Back button handling using dispatcher
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (webView.canGoBack()) {
+                    webView.goBack()
+                } else {
+                    // This removes the callback so the system can handle the back press
+                    isEnabled = false
+                    onBackPressedDispatcher.onBackPressed()
+                }
+            }
+        })
     }
 }
